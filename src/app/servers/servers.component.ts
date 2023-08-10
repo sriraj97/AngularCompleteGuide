93a0cdb97185e065e8e11107733f1c91d4b7e433
sriraj97 <anbuthatha@gmail.com>
2023-08-10 15:01:07 +0530

    No:41:ngFor-Strucural-Directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,12 +10,14 @@ export class ServersComponent implements OnInit {
   serverCreationStatus='No server was created!'
   serverName='TestServer'
   serverCreated=false
+  servers=['TestServer','TestServer 2']
   constructor() { setTimeout(()=>{this.allowNewServer=true},2000);}
 
   ngOnInit(): void {
   }
   onCreateServer(){
     this.serverCreated=true
+    this.servers.push(this.serverName)
     this.serverCreationStatus= 'Server was created! Name is '+this.serverName
   }
   onUpdateServerName(event:Event){
