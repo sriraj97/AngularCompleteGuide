2623e9da4a7a2bac307cec99a1a85ad7169b3d7e
sriraj97 <anbuthatha@gmail.com>
2023-08-10 14:48:35 +0530

    No:40:Using-ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles:['.online{color : white;}']
+  // styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
   serverId:number=10
