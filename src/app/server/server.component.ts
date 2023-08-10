dde98b675087c81afbe2cf31f8d1af801a3a3619
sriraj97 <anbuthatha@gmail.com>
2023-08-10 14:24:25 +0530

    No:39:Property-Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,15 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId:number=10
+  serverStatus:string='offline'
 
-  constructor() { }
+  constructor() { 
+    this.serverStatus=Math.random()>0.5?'online':'offline'
+  }
 
   ngOnInit(): void {
   }
-  serverId:number=10
-  serverStatus:string='offline'
+  
   getServerStatus(){
     return this.serverStatus
   }
 
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red'
+  }
 }
